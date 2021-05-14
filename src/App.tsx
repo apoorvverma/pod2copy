eee019fbf0b57f70e51f90969c5210d89fa57e99
anviiiijain <anvijain250102@gmail.com>
2021-05-14 19:14:50 +0530

    trying both components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
+import Button from './components/shared/Button';
+import Input from './components/shared/Input';
+import Select from './components/shared/Select';
+
+
 function App() {
+  const [text,setText]=useState('')
+
+
+  const handleChange = (key:string) => (value:string) => {
+   console.log(key + ":" + value)
+   setText(value)
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +31,16 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Button buttonStyle="btn--secondary" buttonSize="btn--medium" onClick={e=>e.preventDefault}>Log in</Button>
+      <Button buttonStyle="btn--primary" buttonSize="btn--medium" onClick={e=>e.preventDefault}>Register</Button>
+      <Input 
+        value={text}
+        type='text'
+        placeholder='Enter text here...'
+        onChange={handleChange('text')}
+        />
+       
     </div>
   );
 }
