0774285677b2f3eec2b939a851012b123c9c0bbe
Apoorv Verma <apoorv.verma999@gmail.com>
2021-05-14 18:04:28 +0530

    [AV] Created Navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,19 @@
-const Nav = () =>  {
+const Navbar: React.FC = () =>  {
     return(
-        <div>
-            <p>hello</p>
+        <div className="nav-container">
+            <div className="logo">Hustlers Village</div>
+            <div className="nav-elements">
+                <p>Home</p>
+                <p>How It Works?</p>
+                <p>Contact</p>
+                <button className="Login">Log in</button>
+                <button  className="Register">Register 🡪</button>
+            </div>
+                <svg xmlns="http://www.w3.org/2000/svg" className="icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
         </div>
     )
 }
 
-export default Nav;
+export default Navbar;
