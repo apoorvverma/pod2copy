fb8d1ed915cb8292013697ab6f8650a2fd32ce54
anviiiijain <anvijain250102@gmail.com>
2021-05-15 20:25:13 +0530

    trial.tsx created

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import React,{useState} from 'react';
 import './App.css';
 
-import Button from './components/shared/Button';
-import Input from './components/shared/Input';
-import Select from './components/shared/Select';
-import {IconRightArrow,IconDribbble,IconGoogle} from './components/shared/Icons'
+
 
 
 function App() {
   const [text,setText]=useState('')
 
 
-  const handleChange = (key:string) => (value:string) => {
-   console.log(key + ":" + value)
-   setText(value)
-  };
+
 
 
   return (
@@ -33,63 +27,7 @@ function App() {
           Learn React
         </a>
       </header> */}
-      <Button 
-      buttonStyle="btn--secondary" 
-      buttonSize="btn--medium" 
-      onClick={e=>e.preventDefault}
-      >
-        Log in
-      </Button>
-      <Button 
-      icon={<IconRightArrow/>} 
-      buttonStyle="btn--primary" 
-      buttonSize="btn--medium" 
-      onClick={e=>e.preventDefault}
-      >
-        Register
-      </Button>
-      <Button 
-      icon={<IconDribbble/>} 
-      iconAlign="btn--iconleft" 
-      buttonStyle="btn--dribbble" 
-      buttonSize="btn--medium"
-       onClick={e=>e.preventDefault}
-       >
-         Dribbble
-      </Button>
-      <Button 
-      icon={<IconGoogle/>} 
-      iconAlign="btn--iconleft" 
-      buttonStyle="btn--google" 
-      buttonSize="btn--medium" 
-      onClick={e=>e.preventDefault}
-      >
-        Google
-      </Button>
-      <Button 
-      icon={<IconRightArrow/>} 
-      buttonStyle="btn--blue" 
-      buttonSize="btn--medium" 
-      onClick={e=>e.preventDefault}
-      >
-        Start Hustling
-      </Button><br/>
-      <Button 
-      icon={<IconRightArrow/>} 
-      buttonStyle="btn--primary" 
-      buttonSize="btn--large" 
-      onClick={e=>e.preventDefault}
-      >
-        Take Me To The Village
-      </Button><br/>
-      <Input 
-        value={text}
-        type='text'
-        placeholder='Enter text here...'
-        onChange={handleChange('text')}
-        />
-        <Select data={[{}]} placeholder="select" onChange={()=>console.log("handled change")}/>
-    </div>
+     </div>
   );
 }
 
