f5b41002176783cdfcaa9be7294099865df781b2
aarynverma <18100BTCSE02667@svvvindore.onmicrosoft.com>
2021-05-15 18:35:27 +0530

    optimized login components

--- src/components/login/Inputfield.tsx
+++ src/components/login/Inputfield.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 
 interface abc {
-    inputFieldType: string
+  inputFieldType: string;
+//   subButtonType: string;
 }
 
-export const Inputtypefeild:React.FC <abc> = ({inputFieldType}) => {
-    return (
-        <>
-        <input className="field" type="text" placeholder={inputFieldType === "mail" ? 'Enter your mail' : inputFieldType === "password" ?'Enter your password': 'hello'} />
-        </>
-    )
-}
+export const Inputtypefeild: React.FC<abc> = ({ inputFieldType}) => {
+  return (
+    <>
+      <input
+        className="field"
+        type="text"
+        placeholder={
+          inputFieldType === "mail"
+            ? "Enter your mail"
+            : inputFieldType === "password"
+            ? "Enter your password"
+            : " "
+            //  subButtonType ==="forgot" ? 'Forgot Password ?' :  'Sign Up'
+        }
+      />
+      {/* <div style={{display:"inline"}} >{}</div> */}
+    </>
+  );
+};
--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,45 +1,98 @@
-import {Button} from './Button';
-import {Inputtypefeild} from './Inputfield';
+import Button from './components/Button';
+import Input from './components/Input';
 import {Label} from './Label';
 import {Subbutton} from './Subbutton';
+import {IconRightArrow,IconDribbble,IconGoogle} from './components/Icons';
+import Github from "../../images/github.svg";
 import './login.scss';
 
 
 function Login() {
+    
+        
    
     return (
-        <div className="container">
-            <div className="body">
-                <div className="log">
+        <div className="login-container">
+        <div className="login-card">
+           
+                <h2 className="signup-header">
                 <Label labeltype='head'/>
-                </div>
-                <div className="button">
-                    <Button buttontype='Google' /> 
-                    <Button buttontype='Github' /> 
-                    <Button buttontype='Dribbble' /> 
+                </h2>
+                <div className="social-btns">
+                    <Button 
+                        icon={<IconGoogle/>} 
+                        iconAlign="btn--iconleft" 
+                        buttonStyle="btn--google" 
+                        buttonSize="btn--medium" 
+                        onClick={e=>e.preventDefault}
+                        >
+                        Google
+                    </Button>
+                    
+                    <Button 
+                        icon={<img src={Github} alt="" style={{width:"14px", height:"14px"}  }/>} 
+                        iconAlign="btn--iconleft" 
+                        buttonStyle="btn--github" 
+                        buttonSize="btn--medium"
+                        onClick={e=>e.preventDefault}
+                        >
+                        Github
+                    </Button> 
+                    <Button 
+                        icon={<IconDribbble/>} 
+                        iconAlign="btn--iconleft" 
+                        buttonStyle="btn--dribbble" 
+                        buttonSize="btn--medium"
+                        onClick={e=>e.preventDefault}
+                        >
+                        Dribbble
+                    </Button> 
                 </div>
                 <div className="option">
                     <hr/> 
                         or login with email
                     <hr/>
                 </div>
-                <div className="input">
-                    <Label labeltype='email'/>
-                    <Inputtypefeild inputFieldType='mail' />
-                </div>
-                <div className="input">
-                    <div className="inline">
-                        <Label labeltype='pass' />
-                                
+                <div className="registraton-from">
+                    <form action="" >
                         
-                        <Subbutton subButtonType='forgot' />  
-                    </div>
-                    <Inputtypefeild inputFieldType='password' />
-                </div>
-                <Button buttontype='Arrow' /> 
-                <div className="note">Don't have an account on hustlersVilage? <Subbutton subButtonType='sign' /></div>
-            </div>
+                            <Input 
+                                value=''
+                                type='text'
+                                placeholder='Enter text here...'
+                                label='email'
+                                onChange={(e : any)=>e.preventDefalut()}
+                            />
+                
+                   
+                
+                
 
+                
+                    <Input 
+                        label="password"
+                        value=''
+                        type='text'
+                        subButton={true}
+                        placeholder='Enter text here...'
+                        onChange={(e : any)=>e.preventDefalut()}
+                        />
+                        {/* <Subbutton subButtonType='forgot' />  */}
+                    
+                    <Button 
+                        icon={<IconRightArrow/>} 
+                        buttonStyle="btn--primary" 
+                        buttonSize="btn--large" 
+                        onClick={e=>e.preventDefault}
+                        >
+                        Take Me To The Village
+                    </Button> 
+                <div className="note">Don't have an account on hustlersVilage? <Subbutton subButtonType='sign' /></div>
+                
+                </form>
+                </div>
+                </div>
+            
         </div>
     );
 }
